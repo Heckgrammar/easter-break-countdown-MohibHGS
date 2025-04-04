1aaca7e8e3778c7acb424616ed233a132bea1c3a
Mohib Mohammed <23mmohammed@heckgrammar.co.uk>
2025-04-04 10:15:17 +0100

    Not done yet

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+
+            while (true)
+            {
+                DateTime current = DateTime.Now;
+                DateTime target = new DateTime(2025, 4, 4, 15, 15, 0);
+
+                TimeSpan difference = target - current;
+                Console.WriteLine(difference.Hours + " hours " + difference.Minutes + " minutes " + difference.Seconds + " seconds" + " till we break for easter");
+                Thread.Sleep(1000);
+
+                //while (difference.Seconds < 0) ;
+                //{
+                //    Console.WriteLine("");
+                }
+            }
         }
     }
 }
